f8384ef6654bf2036fdab1b5d87345bc1c38676e
MaxChuloshnikov <89795931+Chuloshnikov@users.noreply.github.com>
2024-08-22 18:50:26 +0300

    done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
-import Image from "next/image";
+import Hero from "@/components/home/Hero";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between">
-      BLOG
+      <Hero/>
     </main>
   );
 }
