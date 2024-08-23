5b6ddd2d22c99616915775cf7209fc789345cd94
MaxChuloshnikov <89795931+Chuloshnikov@users.noreply.github.com>
2024-08-23 14:45:00 +0300

    done

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -2,72 +2,21 @@ import Image from 'next/image';
 import React from 'react';
 import { logo } from "../../assets/images/images";
 import Link from 'next/link';
+import NavbarLinks from './NavbarLinks';
+import { CgMenuGridR } from "react-icons/cg";
 
 const Header = () => {
   return (
-    <header className='py-2 sticky top-0 bg-white z-50'>
+    <header className='py-2 sticky top-0 bg-white z-50 px-2  md:px-4 xl:px-0'>
         <div className='flex justify-between items-center sticky w-full mx-auto'>
-            <nav className=''>
-                <ul className='flex gap-8 items-center text-xl font-medium mt-4'>
-                    <li className='-mt-4'>
-                        <Link 
-                        href={"/"}
-                        className='max-w-max'
-                        >
-                            <h1 
-                            className='font-extrabold text-6xl'
-                            >
-                                Ma
-                            <span className='text-accentBg'>X</span>
-                                ter
-                            </h1>
-                        </Link>
-                    </li>
-                    <li>
-                        <Link 
-                        href={"/"}
-                        className='linkHover'
-                        >
-                            home
-                        </Link>
-                    </li>
-                    <span className='w-4 h-4 bg-accentBg'></span>
-                    <li>
-                        <Link 
-                        href={"/about"}
-                        className='linkHover'
-                        >
-                            
-                            about
-                        </Link>
-                    </li>
-                    <span className='w-4 h-4 bg-accentBg'></span>
-                    <li>
-                        <Link 
-                        href={"/"}
-                        className='linkHover'
-                        >
-                            blog
-                        </Link>
-                    </li>
-                    <span className='w-4 h-4 bg-accentBg'></span>
-                    <li>
-                        <Link 
-                        href={"/contacts"}
-                        className='linkHover'
-                        >
-                            
-                            contacts
-                        </Link>
-                    </li>
-                </ul>
-            </nav>
-            <div>
+           <NavbarLinks/>
+            <div className='flex gap-2'>
                 <button 
-                className='text-xl font-semibold bg-accentBg py-2 px-4 text-white border-2 border-accentBg hover:bg-white hover:text-accentBg duration-200'
+                className='hidden md:block text-xl font-semibold bg-accentBg py-2 px-4 text-white border-2 border-accentBg hover:bg-white hover:text-accentBg duration-200'
                 >
                     Get started
                 </button>
+                <CgMenuGridR className='lg:hidden w-14 h-14 text-accentBg'/>
             </div>
         </div>
        
