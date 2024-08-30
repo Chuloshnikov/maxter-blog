60beba0f704d1c36b9bafb110227f7c3ce804825
MaxChuloshnikov <89795931+Chuloshnikov@users.noreply.github.com>
2024-08-30 11:18:33 +0300

    done

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -8,7 +8,7 @@ const Footer = () => {
         <div className='max-w-contentContainer mx-auto pt-12 pb-20 px-8'>
             <div className='flex gap-4 justify-between'>
                 <GetInTouchForm/>
-                <div className='flex flex-col gap-2 text-center items-center justify-between'>
+                <div className='flex flex-col gap-2 text-center items-center justify-between flex-grow'>
                     <div className='flex gap-1'>
                         <h2 className='font-extrabold text-black text-4xl'>
                             Ma
@@ -21,7 +21,7 @@ const Footer = () => {
                     </div>
                     <p className='text-sm'> &copy; Maxter blog. All Rights Reserved 2024 </p>
                 </div>
-                <div className='px-4'>
+                <div className='flex mx-16 items-center justify-center'>
                     <ul className='flex flex-col gap-2'>
                         <li>
                             <Link href={'/'}>home</Link>
