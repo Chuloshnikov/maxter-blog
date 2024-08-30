663ad8fba2a1fa7d79daf92e3368fc1b8a4aec0a
MaxChuloshnikov <89795931+Chuloshnikov@users.noreply.github.com>
2024-08-30 10:44:43 +0300

    done

---
+++ src/components/footer/GetInTouchForm.tsx
@@ -0,0 +1,27 @@
+import React from 'react'
+
+type Props = {}
+
+const GetInTouchForm = (props: Props) => {
+  return (
+    <div className='flex flex-col gap-4 justify-between'>
+        <h4 className='text-2xl font-semibold'>Get in touch</h4>
+        <form>
+            <input
+            className='px-4 py-2 bg-accentBg border border-white placeholder:text-white'
+            type='email'
+            placeholder='email'
+            />
+            <button
+            className='px-4 py-2 bg-black texct-white hover:bg-white hover:text-accentBg duration-200'
+            type='submit'
+            >
+                Submit
+            </button>
+            
+        </form>
+    </div>
+  )
+}
+
+export default GetInTouchForm
