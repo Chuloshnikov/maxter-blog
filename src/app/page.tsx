4257fc2b2436ea7f9ddb13d7af9def937ad1810b
MaxChuloshnikov <89795931+Chuloshnikov@users.noreply.github.com>
2024-09-14 16:26:48 +0300

    page created, text added
    stylization need

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import About from "@/components/home/About";
 import Hero from "@/components/home/Hero";
 
 export default function Home() {
   return (
     <main className="max-w-contentContainer mx-auto flex min-h-screen flex-col items-center justify-between">
       <Hero/>
+      <About/>
     </main>
   );
 }
