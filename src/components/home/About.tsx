cc7e0411e31699cb2728b1b640a9103689680b8a
MaxChuloshnikov <89795931+Chuloshnikov@users.noreply.github.com>
2024-09-15 08:51:31 +0300

    done

--- src/components/home/About.tsx
+++ src/components/home/About.tsx
@@ -2,10 +2,19 @@ import React from 'react';
 
 const About = () => {
   return (
-    <div>
-        This is a MCH blog, where everyone can share their thoughts or opinions about anything, without censorship.
-        Everything that is published in the blog is the personal opinion of the authors who published the post, responsibility for statements in the address of someone and something is borne directly by the author who published the post. Posts can be on any topic, both IT and other topics that you would like to discuss or raise on the agenda. And remember: Control your thoughts, they can come true.
-    </div>
+    <section className='mt-8'>
+      <div className='p-2'>
+          <p className='text-2xl text-accentBg font-bold max-w-[500px]'>
+            This is a <span className='text-4xl font-extrabold text-black'>MCH</span> blog, 
+            where everyone can share their thoughts or opinions about anything, 
+            without censorship. Everything that is published in the blog is the personal 
+            opinion of the authors who published the post, responsibility for statements 
+            in the address of someone and something is borne directly by the author who 
+            published the post. Posts can be on any topic, both IT and other topics that 
+            you would like to discuss or raise on the agenda. And remember: Control your thoughts, they can come true.
+          </p>
+      </div>
+    </section>
   )
 }
 
