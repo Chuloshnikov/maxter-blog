275afa1eae5330d345a620ea2b76c78b1a9af59f
MaxChuloshnikov <89795931+Chuloshnikov@users.noreply.github.com>
2024-09-15 09:50:03 +0300

    done, cube pozitions fix needed

--- src/components/home/About.tsx
+++ src/components/home/About.tsx
@@ -3,22 +3,22 @@ import AnimationCube from '../animations/AnimationCube';
 
 const About = () => {
   return (
-    <section className='mt-8 flex gap-16 mb-12'>
+    <section className='mt-8 flex flex-col-reverse lg:flex-row gap-12 xl:gap-16 mb-12'>
         <AnimationCube/>
       <div className='p-6 flex flex-col gap-4 max-w-[500px] flex-1'>
-          <p className='text-xl text-accentBg font-bold'>
-            This is a <span className='text-4xl font-extrabold text-black'>MCH</span> blog, 
+          <p className='text-lg md:text-xl text-accentBg font-bold'>
+            This is a <span className='text-2xl md:text-4xl font-extrabold text-black'>MCH</span> blog, 
             where everyone can share their thoughts or opinions about anything, 
             without censorship. Everything that is published in the blog is the personal 
             opinion of the authors who published the post, responsibility for statements 
             in the address of someone and something is borne directly by the author who 
             published the post. 
           </p>
-          <p className='text-xl text-accentBg font-bold'>
+          <p className='text-lg md:text-xl text-accentBg font-bold'>
             Posts can be on any topic, both IT and other topics that 
             you would like to discuss or raise on the agenda.
           </p>
-          <blockquote className='text-2xl font-bold mt-8'>And remember: Control your thoughts, they can come true.</blockquote>
+          <blockquote className='text-lg md:text-2xl font-bold mt-8'>And remember: Control your thoughts, they can come true.</blockquote>
       </div>
     </section>
   )
