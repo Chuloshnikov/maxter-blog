84f1095a6ed323b85c67a1dcd31b1829b6bae4fa
MaxChuloshnikov <89795931+Chuloshnikov@users.noreply.github.com>
2024-09-16 18:38:23 +0300

    done

---
+++ src/app/contacts/page.tsx
@@ -0,0 +1,18 @@
+
+
+export default function Contacts() {
+  return (
+    <main className="max-w-contentContainer mx-auto flex min-h-screen flex-col items-center justify-between">
+        <div>
+            <h2 className="text-2xl max-w-[500px]">
+                For feedback and suggestions, including commercial ones, 
+                you can fill out the form and we will contact you shortly.
+            </h2>
+            <div>
+
+            </div>
+        </div>
+
+    </main>
+  )
+}
