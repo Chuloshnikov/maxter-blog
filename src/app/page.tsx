aafb0cb8ad1ac55663268fcb85d1bc1d5480f6ce
MaxChuloshnikov <89795931+Chuloshnikov@users.noreply.github.com>
2024-09-23 19:59:19 +0300

    done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import About from "@/components/home/About";
 import Advertisement from "@/components/home/Advertisement";
+import Explanations from "@/components/home/Explanations";
 import Hero from "@/components/home/Hero";
 
 export default function Home() {
@@ -7,6 +8,7 @@ export default function Home() {
     <main className="max-w-contentContainer mx-auto flex min-h-screen flex-col items-center justify-between">
       <Hero/>
       <About/>
+      <Explanations/>
       <Advertisement/>
     </main>
   );
---
+++ src/components/home/Explanations.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+const Explanations = () => {
+  return (
+    <section className='py-10 w-full'>
+        <div className='flex items-center justify-center gap-5 w-full bg- bg-gradient-to-r from-[#3DB4FF] to-gray-200 spacey-1'>
+            <div className='flex flex-col gap-12 text-center p-8'>
+                <h4 className='text-4xl font-bold text-white'>
+                    This website was designed to create a community of smart and talented developers.
+                </h4>
+                <blockquote className='text-xl font-semibold'>
+                    - Only we can save this world -
+                </blockquote>
+            </div>
+            <div className='p-8'>
+                <video autoPlay muted loop>
+                    <source src="/content/keyboard.mp4" type="video/mp4" />
+                </video>
+            </div>
+        </div>
+    </section>
+  )
+}
+
+export default Explanations;
