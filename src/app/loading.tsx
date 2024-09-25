3feb511985f4ae03a30a62e982f28b41e3fac4ab
MaxChuloshnikov <89795931+Chuloshnikov@users.noreply.github.com>
2024-09-25 10:15:59 +0300

    done

---
+++ src/app/loading.tsx
@@ -0,0 +1,10 @@
+import Preloader from "@/components/ui/Preloader";
+
+
+export default function Loading() {
+  return (
+    <div className="max-w-contentContainer mx-auto flex items-center justify-center">
+        <Preloader/>
+    </div>
+  )
+}
