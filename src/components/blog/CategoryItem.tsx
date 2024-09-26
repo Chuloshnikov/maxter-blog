eaf6ab955e98001c6dab6a22dbb0652a9f10c9ab
MaxChuloshnikov <89795931+Chuloshnikov@users.noreply.github.com>
2024-09-26 13:40:39 +0300

    done

---
+++ src/components/blog/CategoryItem.tsx
@@ -0,0 +1,35 @@
+import Image from 'next/image';
+import Link from 'next/link';
+import React from 'react'
+
+interface CategoryItemProps {
+    _id: string;
+    title: string;
+    slug: string;
+    img: string;
+}
+
+const CategoryItem = ({category}: {category: CategoryItemProps}) => {
+  return (
+    <li 
+    className='w-[300px] h-[300px] relative hover:opacity-80 duration-200'
+    key={category._id}
+  >
+    <Link href={`/categories/${category.slug}`}>
+      <div className='w-full h-full relative'>
+        <Image 
+          src={category.img} 
+          className='object-cover' 
+          layout='fill'
+          alt={category.title} 
+        />
+        <h4 className='z-10 absolute px-2 py-1 right-0 bottom-0 bg-accentBg text-white'>
+          {category.title}
+        </h4>
+      </div>
+    </Link>
+  </li>
+  )
+}
+
+export default CategoryItem;
