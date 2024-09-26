23d8e9620342e3a9315e9d63019a57aa83dfdfc4
MaxChuloshnikov <89795931+Chuloshnikov@users.noreply.github.com>
2024-09-26 19:11:00 +0300

    blog items fix

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -24,7 +24,7 @@ export default function BlogCategories() {
   return (
     <section className='max-w-contentContainer mx-auto flex flex-col items-center justify-center'>
       <div>
-        <ul className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4'>
+        <ul className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4'>
           {categories?.map((category: any) => <CategoryItem key={category._id} category={category}/>)}
         </ul>
       </div>
