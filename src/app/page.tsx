92409f910ec5af104fd2f85770058b2797c75aac
MaxChuloshnikov <89795931+Chuloshnikov@users.noreply.github.com>
2024-09-26 17:32:14 +0300

    done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,11 +5,11 @@ import Hero from "@/components/home/Hero";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between">
+    <section className="flex min-h-screen flex-col items-center justify-between">
       <Hero/>
       <About/>
       <Explanations/>
       <Advertisement/>
-    </main>
+    </section>
   );
 }
