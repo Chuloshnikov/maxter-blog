05ff33da788f302fee1984e724bf1703e5e20303
MaxChuloshnikov <89795931+Chuloshnikov@users.noreply.github.com>
2024-09-27 16:58:03 +0300

    done

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -5,13 +5,14 @@ import {getServerSession} from "next-auth";
 import {authOptions} from "@/lib/authOptions";
 import ProfileInfoForm from "@/components/profile/ProfileInfoForm";
 import { ProfileInfoModel } from "@/models/ProfileInfo";
+import { redirect } from "next/navigation";
 
 
 export default async function Profile() {
   const session = await getServerSession(authOptions);
 
   if (!session || !session.user?.email) {
-    return 'Not logged in';
+    return redirect('/');
   }
 
   const email = session.user.email;
