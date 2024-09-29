22868f1a7c1ef7e74d208109ea4fec9a0a2189d4
MaxChuloshnikov <89795931+Chuloshnikov@users.noreply.github.com>
2024-09-29 14:02:35 +0300

    done (api comment fix needed)

---
+++ src/models/Comment.ts
@@ -0,0 +1,21 @@
+import { Schema, model, models } from 'mongoose';
+
+
+interface CommentTypes {
+    authorName: string;
+    authorAvatarUrl: string;
+    authorEmail: string;
+    desc: string;
+    postId: string;
+}
+
+
+const commentsSchema = new Schema<CommentTypes>({
+    authorName: { type: String, required: true },
+    authorEmail: { type: String, required: true },
+    authorAvatarUrl: { type: String },
+    desc: { type: String, required: true, unique: true},
+    postId: {type: String},
+  }, {timestamps: true});
+  
+  export const CommentsModel = models?.Comment || model<CommentTypes>('Comment', commentsSchema);
