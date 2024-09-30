7ef555fcdfcaab42cfd8d01608d210e9e5d4342a
MaxChuloshnikov <89795931+Chuloshnikov@users.noreply.github.com>
2024-09-30 14:02:23 +0300

    done

--- src/components/animations/AnimationCube.tsx
+++ src/components/animations/AnimationCube.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const AnimationCube = () => {
   return (
-    <div className='container w-full h-full overflow-hidden p-6 grid grid-cols-2 gap-1 flex-1 mt-8'>
+    <div className='cube-container overflow-hidden p-6 grid grid-cols-2 mt-8'>
       <div className='cube cube-1'></div>
       <div className='cube cube-2'></div>
       <div className='cube cube-3'></div>
