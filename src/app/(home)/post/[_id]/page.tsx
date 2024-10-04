07d65680df8373ca84a46791384c3c146d2089ce
MaxChuloshnikov <89795931+Chuloshnikov@users.noreply.github.com>
2024-10-04 15:33:59 +0300

    views added with localstorage
    for some reason the viewing is recorded twice, possibly and even most likely due to the development mode

--- src/app/(home)/post/[_id]/page.tsx
+++ src/app/(home)/post/[_id]/page.tsx
@@ -1,22 +1,40 @@
-"use client"
+"use client";
 import PostPage from "@/components/blog/PostPage";
 import { useEffect, useState } from "react";
 
-export default function Post({params: {_id}}: SearchParamProps) {
+export default function Post({ params: { _id } }: SearchParamProps) {
+  const [post, setPost] = useState<any>(null);
 
-    const [post, setPost] = useState<any>(null);
-    console.log(post);
+  useEffect(() => {
+    //Check viewed in localstorage
+    let viewedPosts = JSON.parse(localStorage.getItem('viewedPosts') || '[]');
 
-    useEffect(() => {
-      fetch(`/api/post/${_id}`)
+    // Is views is array
+    if (!Array.isArray(viewedPosts)) {
+      viewedPosts = [];
+    }
+
+    //If its first time to viewed
+    if (!viewedPosts.includes(_id)) {
+      // Отправляем запрос для увеличения просмотров
+      fetch(`/api/post/${_id}`, { method: 'PUT' })
         .then((res) => res.json())
-        .then((data) => setPost(data));
-    }, [_id]); 
+        .then(() => {
+          // add to viewed
+          localStorage.setItem('viewedPosts', JSON.stringify([...viewedPosts, _id]));
+        })
+        .catch((err) => console.error('Failed to increment view count', err));
+    }
 
-    return (
-        <section className="min-w-screen min-h-[calc(100vh-21.1rem)] mx-auto flex flex-col mb-8">
-          {post && (<PostPage post={post}/>)}
-        </section>
-    )
+    fetch(`/api/post/${_id}`)
+      .then((res) => res.json())
+      .then((data) => setPost(data))
+      .catch((err) => console.error('Failed to fetch post data', err));
+  }, [_id]);
+
+  return (
+    <section className="min-w-screen min-h-[calc(100vh-21.1rem)] mx-auto flex flex-col mb-8">
+      {post && <PostPage post={post} />}
+    </section>
+  );
 }
-
