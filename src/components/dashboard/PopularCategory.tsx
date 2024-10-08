d7bf9de17b69847695d7ee6de67ef38e4569d1e7
MaxChuloshnikov <89795931+Chuloshnikov@users.noreply.github.com>
2024-10-08 16:09:29 +0300

    data in to graph added (+ PopularCategory logic)

--- src/components/dashboard/PopularCategory.tsx
+++ src/components/dashboard/PopularCategory.tsx
@@ -1,14 +1,16 @@
-import React from 'react'
+import { findMostCommonCategory } from "@/lib/utils";
+import { PostInfo } from "@/models/Post";
 
-const PopularCategory = () => {
+const PopularCategory = ({posts}: any) => {
+  console.log(posts);
   return (
     <div className='border-2 border-gray-500 p-10'>
         <div className='flex items-center justify-center gap-2 text-2xl text-accentBg font-semibold'>
             <span>Most popular category:</span>
-            <span className="capitalize">Games</span>
+            <span className="capitalize">{findMostCommonCategory(posts)}</span>
         </div>
     </div>
   )
 }
 
-export default PopularCategory
+export default PopularCategory;
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,5 +1,39 @@
+import { PostInfo } from "@/models/Post";
+
 export const parseStringify = (value: any) => JSON.parse(JSON.stringify(value));
 
-export function replaceUrlDelimiters(str) {
+export function replaceUrlDelimiters(str: string) {
     return str.replace(/[^a-zA-Z0-9\s]/g, ' ');
 }
+
+
+
+  
+  export function findMostCommonCategory(posts: PostInfo[]): string | null {
+     // If post is array
+  if (!Array.isArray(posts)) {
+    console.error('Expected an array of posts');
+    return null; // Catch err if not
+  }
+
+  // Check all slug
+  const slugCount = posts.reduce<{ [key: string]: number }>((acc, post) => {
+    if (post.slug) {
+      acc[post.slug] = (acc[post.slug] || 0) + 1; // Increase slug count
+    }
+    return acc;
+  }, {});
+
+  // Find max slug 
+  let mostCommonSlug: string | null = null;
+  let maxCount = 0;
+
+  for (const slug in slugCount) {
+    if (slugCount[slug] > maxCount) {
+      maxCount = slugCount[slug];
+      mostCommonSlug = slug;
+    }
+  }
+
+  return mostCommonSlug;
+  }
