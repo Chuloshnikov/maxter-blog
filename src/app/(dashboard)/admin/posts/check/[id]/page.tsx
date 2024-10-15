c3e257a33055e3f4482cb78ba85e4f4ebef342c2
MaxChuloshnikov <89795931+Chuloshnikov@users.noreply.github.com>
2024-10-15 17:57:57 +0300

    pages added (stylization needed)

--- src/app/(dashboard)/admin/posts/check/[id]/page.tsx
+++ src/app/(dashboard)/admin/posts/check/[id]/page.tsx
@@ -1,14 +1,21 @@
+import Author from '@/components/dashboard/Author';
+import TextCheckPage from '@/components/dashboard/TextCheckPage';
 import { PostInfoModel } from '@/models/Post';
 import { ProfileInfoModel } from '@/models/ProfileInfo';
 import mongoose from 'mongoose';
 import React from 'react'
 
-export default async function CheckPost({ params: { _id } }: { params: { _id: string } }) {
+export default async function CheckPost({ params: { id } }: { params: { id: string } }) {
     await mongoose.connect(process.env.MONGODB_URI as string);
-    const post = JSON.parse(JSON.stringify( await PostInfoModel.findOne({_id})));
-    const user = JSON.parse(JSON.stringify( await ProfileInfoModel.findOne({ _id: post.authorId})));
-    
+    const post = JSON.parse(JSON.stringify( await PostInfoModel.findOne({_id: id})));
+    const user = JSON.parse(JSON.stringify( await ProfileInfoModel.findOne({ _id: post?.authorId})));
+    console.log(id);
+    console.log(post?.authorId);
+    console.log(user);
   return (
-    <div>Check post</div>
+    <div>
+        <Author user={user}/>
+        <TextCheckPage text={post.desc}/>
+    </div>
   )
 }
