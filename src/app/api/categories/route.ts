263197aae5e82697d154e919326e894c1e6634c7
MaxChuloshnikov <89795931+Chuloshnikov@users.noreply.github.com>
2024-10-20 15:19:41 +0300

    done

--- src/app/api/categories/route.ts
+++ src/app/api/categories/route.ts
@@ -1,6 +1,9 @@
 import { NextResponse } from 'next/server';
 import mongoose from "mongoose";
 import Category from '@/models/Category';
+import {getServerSession} from "next-auth";
+import { authOptions } from '@/lib/authOptions';
+import { ProfileInfoModel } from '@/models/ProfileInfo';
 
 export async function GET() {
 await mongoose.connect(process.env.MONGODB_URI as string);
@@ -8,7 +11,30 @@ const categories = await Category.find({});
 return NextResponse.json(categories);
 }
 
-export async function POST() {
+export async function POST(request: Request) {
 await mongoose.connect(process.env.MONGODB_URI as string);
+
+const { title, imgUrl } = await request.json();
+
+try {
+    const session = await JSON.parse(JSON.stringify(await getServerSession(authOptions)));
+    
+    if (!session) {
+      return NextResponse.json({ message: 'Not authenticated' }, { status: 401 });
+    }
+
+    const email = session.user.email;
+    const profile = await ProfileInfoModel.findOne({ email });
+    
+    if (profile && profile.admin) {
+      const categoryDoc = await Category.create({title, img: imgUrl, slug: title });
+      return NextResponse.json(categoryDoc);
+    } else {
+      return NextResponse.json({ message: 'Forbidden: You do not have rights for this action' }, { status: 403 });
+    }
+  } catch (error) {
+    console.error('Error creating category:', error);
+    return NextResponse.json({ message: 'Internal Server Error' }, { status: 500 });
+  }
 
 }
