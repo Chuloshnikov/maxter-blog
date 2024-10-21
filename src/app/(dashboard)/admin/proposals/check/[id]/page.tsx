cb93243f63d9446d6dd4ed3598ab2ee8e7a52c4e
MaxChuloshnikov <89795931+Chuloshnikov@users.noreply.github.com>
2024-10-21 13:04:50 +0300

    +

--- src/app/(dashboard)/admin/proposals/check/[id]/page.tsx
+++ src/app/(dashboard)/admin/proposals/check/[id]/page.tsx
@@ -1,7 +1,13 @@
+import { ContactsModel } from "@/models/Contacts";
+import mongoose from "mongoose";
 
 
-export default function CheckProposal({ params: { id } }: { params: { id: string } }) {
+export default async function CheckProposal({ params: { id } }: { params: { id: string } }) {
+    await mongoose.connect(process.env.MONGODB_URI as string);
+    const contactInfo = JSON.parse(JSON.stringify( await ContactsModel.findById(id)));
   return (
-    <div>{id}</div>
+    <div>
+        {id}
+    </div>
   )
 }
