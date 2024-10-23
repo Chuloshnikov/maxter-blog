72a01e0535aa8abca629f584e89e2655a4623407
MaxChuloshnikov <89795931+Chuloshnikov@users.noreply.github.com>
2024-10-23 10:22:08 +0300

    fixes done

--- src/app/(dashboard)/admin/posts/page.tsx
+++ src/app/(dashboard)/admin/posts/page.tsx
@@ -10,7 +10,7 @@ export default async function Posts() {
       <ItemsContainer 
        page={"posts"} 
        action={"check"} 
-      items={allPosts} 
+      items={allPosts.reverse()} 
       slug={"all posts"}
       />
     </div>
