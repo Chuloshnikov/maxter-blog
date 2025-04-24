be7041843d011525f7bb7d8670e7ac5dfa71df08
MaxChuloshnikov <89795931+Chuloshnikov@users.noreply.github.com>
2025-04-24 13:17:47 +0300

    done

--- src/app/(home)/post/[_id]/page.tsx
+++ src/app/(home)/post/[_id]/page.tsx
@@ -1,40 +1,11 @@
-"use client";
-import PostPage from "@/components/blog/PostPage";
-import { useEffect, useState } from "react";
+import PostPageContainer from "@/components/blog/PostPageContainer";
+
 
 export default function Post({ params: { _id } }: SearchParamProps) {
-  const [post, setPost] = useState<any>(null);
-
-  useEffect(() => {
-    //Check viewed in localstorage
-    let viewedPosts = JSON.parse(localStorage.getItem('viewedPosts') || '[]');
-
-    // Is views is array
-    if (!Array.isArray(viewedPosts)) {
-      viewedPosts = [];
-    }
-
-    //If its first time to viewed
-    if (!viewedPosts.includes(_id)) {
-      // Отправляем запрос для увеличения просмотров
-      fetch(`/api/post/${_id}`, { method: 'PUT' })
-        .then((res) => res.json())
-        .then(() => {
-          // add to viewed
-          localStorage.setItem('viewedPosts', JSON.stringify([...viewedPosts, _id]));
-        })
-        .catch((err) => console.error('Failed to increment view count', err));
-    }
-
-    fetch(`/api/post/${_id}`)
-      .then((res) => res.json())
-      .then((data) => setPost(data))
-      .catch((err) => console.error('Failed to fetch post data', err));
-  }, [_id]);
-
+ 
   return (
     <section className="min-w-screen min-h-[calc(100vh-21.1rem)] mx-auto flex flex-col mb-8">
-      {post && <PostPage post={post} />}
+      <PostPageContainer postId={_id} />
     </section>
-  );
+  )
 }
---
+++ src/components/blog/PostPageContainer.tsx
@@ -0,0 +1,43 @@
+"use client"
+import {useState, useEffect } from "react";
+import PostPage from "./PostPage";
+import { StringDecoder } from "string_decoder";
+
+const PostPageContainer = ({ postId }: any) => {
+    const [post, setPost] = useState<any>(null);
+
+    useEffect(() => {
+      //Check viewed in localstorage
+      let viewedPosts = JSON.parse(localStorage.getItem('viewedPosts') || '[]');
+  
+      // Is views is array
+      if (!Array.isArray(viewedPosts)) {
+        viewedPosts = [];
+      }
+  
+      //If its first time to viewed
+      if (!viewedPosts.includes(postId)) {
+        // Отправляем запрос для увеличения просмотров
+        fetch(`/api/post/${postId}`, { method: 'PUT' })
+          .then((res) => res.json())
+          .then(() => {
+            // add to viewed
+            localStorage.setItem('viewedPosts', JSON.stringify([...viewedPosts, postId]));
+          })
+          .catch((err) => console.error('Failed to increment view count', err));
+      }
+  
+      fetch(`/api/post/${postId}`)
+        .then((res) => res.json())
+        .then((data) => setPost(data))
+        .catch((err) => console.error('Failed to fetch post data', err));
+    }, [postId]);
+  
+    return (
+    <>
+        {post && <PostPage post={post} />}
+    </>
+    );
+}
+
+export default PostPageContainer
